f5415df18fcea98625e820095953b18658cf2a5b
Gabriela Marques de Rangel Moreira <gmrm@windows.cin.ufpe.br>
2025-06-04 14:55:30 -0300

    Andares implementados
    Implementei a máquina de estados e precisa implementar os delays

--- elevadorarduino/elevadorarduino.ino
+++ elevadorarduino/elevadorarduino.ino
@@ -8,6 +8,10 @@ const int botao_t = 2;
 const int botao_1 = 3;
 const int botao_2 = 4;
 const int botao_3 = 5;
+enum Estado {PARADO, ANDANDO} ;
+Estado estado_atual = PARADO;
+int andar_atual = 0;
+int andar_desejado = 0;
 
 void setup() {
   pinMode(botao_t, INPUT);
@@ -44,36 +48,123 @@ void pararElevador()
 }
 
 void loop() {
-  // botao um
-  if (digitalRead(botao_1) == LOW)
+  switch(estado_atual)
+  {
+    case PARADO:
+      if (digitalRead(botao_t) == LOW)
+      {
+        andar_desejado = 0;
+        if (andar_desejado > andar_atual)
+        {
+          subirElevador();
+          estado_atual = ANDANDO;
 
-  { 
-    Serial.print("apertou botao 1\n");
-    // Se ainda não chegou no teto
-    if (digitalRead(fim_de_curso_3) == HIGH){
+        }
+        else if (andar_desejado < andar_atual)
+        {
+          descerElevador();
+          estado_atual = ANDANDO;
 
-    
-      subirElevador();
-      Serial.print("subindo\n");
-    }
-    else
-      pararElevador();
-  }
-  // botao dois
-  else if (digitalRead(botao_2) == LOW)
-  { 
-    Serial.print("apertou botao 2\n");
+        }  
+      }
 
-    // Se ainda não chegou no terreo
-    if (digitalRead(fim_de_curso_t) == HIGH){
+      else if (digitalRead(botao_1) == LOW)
+      {
+        andar_desejado = 1;
+        if (andar_desejado > andar_atual)
+        {
+          subirElevador();
+          estado_atual = ANDANDO;
 
-    
-      descerElevador();
-      Serial.print("descendo\n");
+        }
+        else if (andar_desejado < andar_atual)
+        {
+          descerElevador();
+          estado_atual = ANDANDO;
 
-    }
+        }
+      }
 
-    else
-      pararElevador();
+      else if (digitalRead(botao_2) == LOW)
+      {
+        andar_desejado = 2;
+        if (andar_desejado > andar_atual)
+        {
+          subirElevador();
+          estado_atual = ANDANDO;
+
+        }
+        else if (andar_desejado < andar_atual)
+        {
+          descerElevador();
+          estado_atual = ANDANDO;
+
+        }
+      }
+
+      else if (digitalRead(botao_3) == LOW)
+      {
+        andar_desejado = 3;
+        if (andar_desejado > andar_atual)
+        {
+          subirElevador();
+          estado_atual = ANDANDO;
+
+        }
+        else if (andar_desejado < andar_atual)
+        {
+          descerElevador();
+          estado_atual = ANDANDO;
+
+        }        
+      }
+
+      break;
+    case ANDANDO:
+      if (andar_desejado == 0)
+      {
+        if (digitalRead (fim_de_curso_t) == LOW)
+        {
+          pararElevador();
+          estado_atual = PARADO;
+          andar_atual = 0;
+
+        }
+      }
+
+      else if (andar_desejado == 1)
+      {
+        if (digitalRead (sensor_proximidade_1) == LOW)
+        {
+          pararElevador();
+          estado_atual = PARADO;
+          andar_atual = 1;
+
+        }
+      }
+
+      else if (andar_desejado == 2)
+      {
+        if (digitalRead (sensor_proximidade_2) == LOW)
+        {
+          pararElevador();
+          estado_atual = PARADO;
+          andar_atual = 2;
+        }
+      }
+
+      else if (andar_desejado == 3)
+      {
+        if (digitalRead (fim_de_curso_3) == LOW)
+        {
+          pararElevador();
+          estado_atual = PARADO;
+          andar_atual = 3;
+        }
+      }
+
+      break;
+    default:
+      break;
   }
 }
